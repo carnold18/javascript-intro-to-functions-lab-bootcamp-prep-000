72468663ca4750bbc71c5bbfd21d8021f8553eba
Christopher Arnold <chrisarnold2010@gmail.com>
2018-05-30 23:33:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ function sayHiToGrandma(string)
   if(string === string.toLowerCase) {
     return "I can't hear you!";
   }
-  else if {
+  else if (string === string.toUpperCase) {
+    return "YES INDEED!"
+  }
     return ""
   }
 }
