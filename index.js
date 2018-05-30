bc62dbbe2560b422c1f50bec2bba522004b948ca
Christopher Arnold <chrisarnold2010@gmail.com>
2018-05-30 23:34:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,8 +24,10 @@ function sayHiToGrandma(string)
     return "I can't hear you!";
   }
   else if (string === string.toUpperCase) {
-    return "YES INDEED!"
+    return "YES INDEED!";
   }
-    return ""
+  else if (string === "I love you, Grandma.")
+  {
+    return "I love you, too.";
   }
-}
+  }
