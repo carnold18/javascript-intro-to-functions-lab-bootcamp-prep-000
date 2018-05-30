b79883f7e83521b68b2cb37bdfbc33143f2ecdeb
Christopher Arnold <chrisarnold2010@gmail.com>
2018-05-30 23:32:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function logWhisper(string)
 
 function sayHiToGrandma(string)
 {
-  if(string === lowercase) {
-    return "I can't hear you!"
+  if(string === string.toLowerCase) {
+    return "I can't hear you!";
   }
   else if {
     return ""
